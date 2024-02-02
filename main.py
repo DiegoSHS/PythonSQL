50e018f1ee23938e6b61cca068a58d0d7ac12ff2
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2024-02-02 01:48:03 -0600

    successfully created connection

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
 import pymysql
-from pymysql import Error,Connection
+from pymysql import Connection
 
-def connex(options):
+
+def connex(options: dict):
     try:
-        conection = pymysql.connect(**options)
-        return conection
+        connection = pymysql.connect(**options)
+        print(connection)
+        return connection
     except pymysql.Error as err:
         print(err)
         return
 
-def selectAll(conection:Connection,table:str):
-    cursor = conection.cursor()
-    cursor.execute(f"SELECT * FROM {table}")
+
+def select_all(connection: Connection, table: str):
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM {table};")
     rows = cursor.fetchall()
     return rows
 
-def close(cursor):
+
+def close(connection: Connection):
+    cursor = connection.cursor()
     cursor.commit()
     cursor.close()
-    
-def insertMany(conection:Connection,itemsList):
-    cursor = conection.cursor()
-    for item in itemsList:
+
+
+def insert_many(connection: Connection, items_list: list):
+    cursor = connection.cursor()
+    for item in items_list:
         cursor.intert(**item)
+
+
+separator = ', '
+
+
+def insert_one(connection: Connection, table: str, item: dict):
+    cursor = connection.cursor()
+    cursor.execute(f"INSERT INTO {table} ({separator.join(item.keys())}) values ({separator.join(item.values())})")
+
+
+command = f"CREATE TABLE products (id INT PRIMARY KEY, name VARCHAR(30), description VARCHAR(200),price DOUBLE)"
+conn_options = {"user": 'diego', "host": 'localhost', "password": 'Dla.2002/mysql', "database": "products"}
+
+
+def main():
+    connection = connex(conn_options)
+    rows = select_all(connection=connection, table='products')
+    print(rows)
+    for row in rows:
+        print(row)
+
+
+main()
